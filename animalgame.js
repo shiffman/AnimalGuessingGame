09fa0230f0bbd6db7743c3f3e1e7fafbcf524db7
Daniel Shiffman <daniel@shiffman.net>
2016-12-28 20:16:58 -0500

    comments

--- animalgame.js
+++ animalgame.js
@@ -1,3 +1,10 @@
+// 8 nights of Hanukkah 2016 Examples
+// Night 5: Animal Guessing Game
+// https://www.reddit.com/r/dailyprogrammer/comments/34asls/20150429_challenge_212_intermediate_animal_guess/
+// Daniel Shiffman
+// http://codingrainbow.com/
+
+// Node has data and a yes (left) and no (right) answer
 function Node(data, y, n) {
   this.data = data;
   this.yes = y;
@@ -7,42 +14,55 @@ function Node(data, y, n) {
 var readlineSync = require('readline-sync');
 var fs = require("fs");
 
+// Read in an animal decision tree
 var tree = fs.readFileSync('tree.json');
 var root = JSON.parse(tree);
 var node;
 
 console.log('Welcome to the animal game!');
 
+// Play the game
 while (ask("Do you want to play?")) {
   node = root;
   go();
 }
 
 function go() {
+  // If it's not a "terminal" node (i.e. animal)
   while (node.yes && node.no) {
+    // Ask the question: Yes or No?
     if (ask(node.data)) {
       node = node.yes;
     } else {
       node = node.no;
     }
   }
+  // We're at the end, guess!
   if (!ask("Is it a " + node.data + "?")) {
+    // Wrong!
     train(node);
   } else {
+    // Right!
     console.log("Yay!");
   }
 }
 
+// Ask a question, return true for yes
 function ask(question) {
   var answer = readlineSync.question(question + " (y/n): ").toUpperCase();
   return (answer.charAt(0) == "Y");
 }
 
+// Train a node to get the right answer
 function train(node) {
+  // The wrong guess
   var guess = node.data;
+  // What is it?
   var answer = readlineSync.question("Ok, what are you? ");
+  // Get a new question?
   var question = readlineSync.question("Suggest a yes/no question to distinguish a " + guess + " from a " + answer + ".\n");
   node.data = question;
+  // Yes or no for that question
   if (ask("Answer for a " + answer + ": " + question)) {
     node.yes = new Node(answer);
     node.no = new Node(guess);
@@ -50,6 +70,7 @@ function train(node) {
     node.yes = new Node(guess);
     node.no = new Node(answer);
   }
+  // Save back to the file
   var tree = JSON.stringify(root, null, 2);
   fs.writeFileSync('tree.json', tree);
 }
